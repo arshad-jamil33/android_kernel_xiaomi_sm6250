9fdaf37bf63aeb7925c9050d74c1773a978ac6b7
KimChi <arshadjamil709@gmail.com>
2026-05-03 15:57:05 +0000

    Welcome to Nothing

--- build.sh
+++ build.sh
@@ -40,7 +40,7 @@ echo -e " Kernel Compile Successful"
 git clone --depth=1 https://github.com/Amritorock/AnyKernel3 -b r5x AnyKernel
 cp out/arch/arm64/boot/Image.gz AnyKernel
 cd AnyKernel
-zip -r9 BlackHole-miatoll-${TANGGAL}.zip *
+zip -r9 BlackHole-v2.1_KSU-miatoll-${TANGGAL}.zip *
 cd ../
 fi
 }
